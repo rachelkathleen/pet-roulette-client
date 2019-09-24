345529bc4ab4dc0f8aa78b781ee78bb5d250abd5
rachelkathleen <rachel.hawa@gmail.com>
2019-09-24 15:50:31 -0500

    add pet container and import to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PetContainer from './containers/PetContainer'
 import logo from './logo.svg';
 import './App.css';
 
---
+++ src/containers/PetContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class PetContainer extends React.Component {
+
+ 
+
+  render() {
+      return (
+          <div>
+            will render random pet, button to shuffle pet, and button to open form
+          </div>
+      )
+  }
+
+}
+
+export default PetContainer;
