ed871c09bc273a999ae9801286efaa3578a954dd
rachelkathleen <rachel.hawa@gmail.com>
2019-09-25 00:05:07 -0500

    change to class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import PetContainer from './containers/PetContainer'
 import logo from './logo.svg';
 import './App.css';
+import { getPets } from "./redux/actions/pets";
 
-function App() {
-  return (
-    <div className="App">
-      <PetContainer />
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    this.props.getPets();
+  }
+  render() {
+    return (
+      <div className="App">
+       <PetContainer />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(
+  null,
+  { getPets }
+)(App);
+
+
