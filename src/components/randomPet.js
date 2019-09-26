c5abf0136a9db79c63ed2cb4d6a5b7d43e8e1c37
rachelkathleen <rachel.hawa@gmail.com>
2019-09-25 23:42:37 -0500

    add mapstate to props and update render function

--- src/components/randomPet.js
+++ src/components/randomPet.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { fetchRandomPet } from "../redux/actions/pets";
 
-class showPet extends Component {
+class RandomPet extends Component {
     render() {
-        return (
-            <div>
-                
-            </div>
+        const { pet } = this.props;
+      if (!pet) {
+        return <p>Loading...</p>};
+      return (
+        <div>
+            <h1>Will be a pet!</h1>
+            <p>{pet.name}</p>            
+        </div>
         )
     }
 }
 
-export default connect (null, { fetchRandomPet })(showPet)
+  const mapStateToProps = (state, props) => { debugger
+    const pet = fetchRandomPet
+    return {
+      pet,
+    };
+  };
+
+
+  export default connect(mapStateToProps)(RandomPet);
