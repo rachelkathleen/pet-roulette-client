af8e0f34276d30ed83ee5594ff1162e85fd2766c
rachelkathleen <rachel.hawa@gmail.com>
2019-09-25 23:43:16 -0500

    add dispatch to end of fetchRandomPet

--- src/redux/actions/pets.js
+++ src/redux/actions/pets.js
@@ -1,6 +1,5 @@
 import client from '../../petfinderConfig';
 
-
 const displayPet = pet => {
     return {
       type: "RANDOM_PET_SUCCESS",
@@ -8,17 +7,16 @@ const displayPet = pet => {
     };
   };
   
-  export const fetchRandomPet = () => { 
-    let randomPetIndex = Math.floor(Math.random() * 100)
-    return dispatch => {
-      return client.animal.search({ limit: 100 })
-        .then(resp => {
-            const randomPet = resp.data.animals[randomPetIndex]
-            debugger
-            return randomPet
-        })
-    };
+export const fetchRandomPet = () => { 
+  let randomPetIndex = Math.floor(Math.random() * 100)
+  return dispatch => {
+    return client.animal.search({ limit: 100 })
+      .then(resp => {
+          const randomPet = resp.data.animals[randomPetIndex]
+          return randomPet
+      }).then(pet => dispatch(displayPet(pet)));
   };
+};
   
 //   randomPet.photos[0].medium
 
