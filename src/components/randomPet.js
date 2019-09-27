78305fe5fd0aee4e01e57108117e43775c93a6b8
rachelkathleen <rachel.hawa@gmail.com>
2019-09-26 22:27:35 -0500

    fix indentation, move unused code

--- src/components/randomPet.js
+++ src/components/randomPet.js
@@ -1,29 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
-import { fetchRandomPet } from "../redux/actions/pets";
 
 class RandomPet extends Component {
-    render() { 
-        const pet = this.props.pet.petsReducer;
-        
-      if (pet === []) {
-          return <p>Loading...</p>;
-      }
-      return (
-        <div>
-          <h1>This is a pet (yayyyyy!)</h1> 
-            <p>{pet.name}</p>  
-            <p>{pet.id}</p>  
-        </div>
-        )
+  render() { 
+      const pet = this.props.pet.petsReducer;
+      
+    if (pet === []) {
+        return <p>Loading...</p>;
     }
+    return (
+      <div>
+        <h1>This is a pet (yayyyyy!)</h1> 
+          <p>{pet.name}</p>  
+          <p>{pet.id}</p>  
+      </div>
+      )
+  }
 }
 
-  const mapStateToProps = state => { 
-    return { 
-      pet: state
-    };
+const mapStateToProps = state => { 
+  return { 
+    pet: state
   };
+};
 
-
-  export default connect(mapStateToProps)(RandomPet);
+export default connect(mapStateToProps)(RandomPet);
