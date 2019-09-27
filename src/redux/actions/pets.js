28837458f5f08cf05db5a72831a2ae5c015136b6
rachelkathleen <rachel.hawa@gmail.com>
2019-09-26 21:35:29 -0500

    remove debugger

--- src/redux/actions/pets.js
+++ src/redux/actions/pets.js
@@ -11,7 +11,7 @@ export const fetchRandomPet = () => {
   let randomPetIndex = Math.floor(Math.random() * 100)
   return dispatch => {
     return client.animal.search({ limit: 100 })
-      .then(resp => { 
+      .then(resp => {
           const randomPet = resp.data.animals[randomPetIndex]
           return randomPet
       }).then(pet => dispatch(displayPet(pet)));
