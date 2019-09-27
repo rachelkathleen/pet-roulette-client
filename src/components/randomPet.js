992abbbe2aa3905223aa697df289284c6513f77e
rachelkathleen <rachel.hawa@gmail.com>
2019-09-26 21:07:47 -0500

    fix connection to store, remove debuggers

--- src/components/randomPet.js
+++ src/components/randomPet.js
@@ -3,21 +3,21 @@ import { connect } from "react-redux";
 import { fetchRandomPet } from "../redux/actions/pets";
 
 class RandomPet extends Component {
-    render() { debugger
-        const { pet } = this.props;
+    render() {
+        const pet = this.props.pet.petsReducer;
       if (!pet) {
         return <p>Loading...</p>};
       return (
         <div>
-          <h1>Will be a pet!</h1>
-            
-                
+          <h1>Will be a pet!</h1> 
+            <p>{pet.name}</p>  
+            <p>{pet.id}</p>    
         </div>
         )
     }
 }
 
-  const mapStateToProps = state => {
+  const mapStateToProps = state => { 
     return { 
       pet: state
     };
