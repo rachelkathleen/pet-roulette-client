2bb272f2183b9f54f33f51fba6554fc81515f31d
rachelkathleen <rachel.hawa@gmail.com>
2019-09-28 18:55:54 -0500

    add skepton css

--- src/components/randomPet.js
+++ src/components/randomPet.js
@@ -1,33 +1,43 @@
-import React, { Component } from 'react'
-import { connect } from "react-redux";
+import React, { Component } from 'react'
+import { connect } from "react-redux";
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-class RandomPet extends Component {
-  render() { 
-    const pet = this.props.pet.petsReducer;
-    debugger
-    if (pet.photos !== undefined) {
-      return (
-        <div>
-          <h1>This is a pet (yayyyyy!)</h1> 
-            <p>{pet.name}</p>  
-            <img src={pet.photos[0].medium} alt=""/>
-            <a href={pet.url}>Visit my Page</a>
-             
+class RandomPet extends Component {
+  render() { 
+    const pet = this.props.pet.petsReducer;
+    debugger
+    if (pet.photos !== undefined) {
+      return (
+  <>
+        <div>
+          <h1>{pet.name}</h1>  
+            <img src={pet.photos[0].medium} alt="" />          
+        </div>
+        <div class="row">
+          <div class="six columns"><a class="button" href={pet.url}>Visit me on Petfinder!</a></div>
+         
         </div>
-        )
-    }
-    if (pet.photos === undefined) {
-        return (
-          <img src="https://66.media.tumblr.com/tumblr_lztjlvnBoa1qcay1ao1_500.gif" alt=""/>
-        );
-    } 
-  }
+        <div class="row">
+          
+          <div class="six columns"><a class="button" href={pet.url}>Share me!</a></div>
+        </div>
+</>
+
+        )
+    }
+    if (pet.photos === undefined) {
+        return (
+          <img src="https://66.media.tumblr.com/tumblr_lztjlvnBoa1qcay1ao1_500.gif" alt=""/>
+        );
+    } 
+  }
 }
 
-const mapStateToProps = state => { 
-  return { 
-    pet: state
-  };
+const mapStateToProps = state => { 
+  return { 
+    pet: state
+  };
 };
 
-export default connect(mapStateToProps)(RandomPet);
+export default connect(mapStateToProps)(RandomPet);
