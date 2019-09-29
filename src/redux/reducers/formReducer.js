bf956ab08ce843c46b836ce0bf38e14e953d587b
rachelkathleen <rachel.hawa@gmail.com>
2019-09-28 20:44:28 -0500

    add case for SEND_PET

--- src/redux/reducers/formReducer.js
+++ src/redux/reducers/formReducer.js
@@ -1,6 +1,9 @@
 function formReducer(state = {emails: []}, action) {
     switch(action.type) {
+      case "SEND_PET":
+        debugger
 
+        return action.payload;
       default:
         return state;
     }
