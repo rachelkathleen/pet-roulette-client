ece6f58dc7f0d11ba9109edb0069d230ddd52ef9
rachelkathleen <rachel.hawa@gmail.com>
2019-09-30 16:03:09 -0500

    add loading action, edit default state

--- src/redux/reducers/petReducer.js
+++ src/redux/reducers/petReducer.js
@@ -1,8 +1,22 @@
-export default (state = [], action) => {
+export default (state = 
+  {
+    loading: true, 
+    pet: {photos: []}
+    }, action) => {
   switch (action.type) { 
     case "RANDOM_PET_SUCCESS":
-      return action.pet; 
+      return {
+        ...state, 
+        loading: false,
+        pet: action.pet 
+      } 
+    case "LOADING_PET":
+      return {
+        ...state,
+        loading: true
+      }
     default:
       return state;
   }
 };
+
