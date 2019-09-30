5619db9b896e9d506e9605f32138a143bc5c2600
rachelkathleen <rachel.hawa@gmail.com>
2019-09-30 16:02:23 -0500

    update order, update mapstatetoprops

--- src/components/randomPet.js
+++ src/components/randomPet.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
+import { fetchRandomPet } from "../redux/actions/pets";
 import Media from 'react-bootstrap/Media'
 import Card from 'react-bootstrap/Card'
-// import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import ModalButton from './ModalButton'
+import ModalButton from './ModalButton';
 
 class RandomPet extends Component {
+
+  shufflePet = event => {
+    event.preventDefault();
+    this.props.fetchRandomPet();
+  };
+  
   render() { 
-    const pet = this.props.pet.petsReducer;
-    if (pet.photos !== undefined && pet.photos[0].full !== undefined) {
-      return (
+  const loading = this.props.loading;
+    if (loading) {
+        return (
+          <Container style={{marginTop: '100px'}}>
+              <Row className="justify-content-md-center">
+                <Col md="auto">
+                  <Card border="dark" style={{ width: '25rem'}}>
+                    <Card.Img 
+                      className="mr-3"
+                      src="https://66.media.tumblr.com/tumblr_lztjlvnBoa1qcay1ao1_500.gif"
+                      alt=""
+                    />
+                    <Card.Body>
+                      <Card.Text>
+                        Loading...
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+        );
+    } 
+    const pet = this.props.pet;
+ 
+    return (
     <Container style={{marginTop: '100px'}}>
       <Row className="justify-content-md-center">
         <Col md="auto">
@@ -23,7 +53,7 @@ class RandomPet extends Component {
               />
             <Card.Body>
               <Card.Title>{pet.name}</Card.Title>
-              <button>Shuffle!</button><br/>
+              <button onClick={ event => this.shufflePet(event) }>Shuffle!</button><br/>
               <button><a href={pet.url}>Visit me on Petfinder!</a></button>
               <ModalButton />
             </Card.Body>
@@ -33,26 +63,15 @@ class RandomPet extends Component {
     </Container>
         )
     }
-    if (pet.photos === undefined) {
-        return (
-  <Media>
-  <img
-    width={300}
-    style={{margin: '20px'}}
-    // height={64}
-    className="mr-3"
-    src="https://66.media.tumblr.com/tumblr_lztjlvnBoa1qcay1ao1_500.gif"
-    alt="Generic placeholder"
-  /></Media>
-        );
-    } 
-  }
+    
+  
 }
 
 const mapStateToProps = state => { 
   return { 
-    pet: state
+    pet: state.petReducer.pet,
+    loading: state.petReducer.loading
   };
 };
 
-export default connect(mapStateToProps)(RandomPet);
+export default connect(mapStateToProps, { fetchRandomPet })(RandomPet);
