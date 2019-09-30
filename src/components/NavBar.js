e1e451a4b184a5f4b1465ce0b94cc3599c038cb9
rachelkathleen <rachel.hawa@gmail.com>
2019-09-29 22:24:42 -0500

    create navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function NavBar() {
     return (
-        <div>
-            
-        </div>
+      <Navbar fixed="top" bg="dark" variant="dark">
+        <Navbar.Brand className="nav" href="/">Pet Roulette!</Navbar.Brand>
+          <Link to="/about">About</Link>  
+      </Navbar>
     )
 }
