d5d451ba10c72f40a887987ee1f0f11da1f254cb
rachelkathleen <rachel.hawa@gmail.com>
2019-09-30 12:30:56 -0500

    remove bootstrap buttons, add mrgin

--- src/components/randomPet.js
+++ src/components/randomPet.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from "react-redux";
 import Media from 'react-bootstrap/Media'
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+// import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -11,20 +11,20 @@ import ModalButton from './ModalButton'
 class RandomPet extends Component {
   render() { 
     const pet = this.props.pet.petsReducer;
-    if (pet.photos !== undefined) {
+    if (pet.photos !== undefined && pet.photos[0].full !== undefined) {
       return (
-    <Container>
+    <Container style={{marginTop: '100px'}}>
       <Row className="justify-content-md-center">
         <Col md="auto">
           <Card border="dark" style={{ width: '25rem'}}>
-            <Card.Img variant="top" src={pet.photos[0].medium} />
+            <Card.Img 
+              variant="top" 
+              src={pet.photos[0].full}
+              />
             <Card.Body>
               <Card.Title>{pet.name}</Card.Title>
-              <Card.Text>
-                Blah Blah Blah
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button><br/>
-              <Button variant="primary">Go somewhere</Button>
+              <button>Shuffle!</button><br/>
+              <button><a href={pet.url}>Visit me on Petfinder!</a></button>
               <ModalButton />
             </Card.Body>
           </Card>
@@ -38,6 +38,7 @@ class RandomPet extends Component {
   <Media>
   <img
     width={300}
+    style={{margin: '20px'}}
     // height={64}
     className="mr-3"
     src="https://66.media.tumblr.com/tumblr_lztjlvnBoa1qcay1ao1_500.gif"
