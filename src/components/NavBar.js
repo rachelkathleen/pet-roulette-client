9fc46bf8a6029bf0be6971c1c56d35ad0ed17641
rachelkathleen <rachel.hawa@gmail.com>
2019-09-30 16:01:53 -0500

    remove links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export default function NavBar() {
     return (
       <Navbar fixed="top" bg="dark" variant="dark">
         <Navbar.Brand className="nav" href="/">Pet Roulette!</Navbar.Brand>
-          <Link to="/about">About</Link>  
+           
       </Navbar>
     )
 }
