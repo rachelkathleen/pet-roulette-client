f4095878b0af7102fe597300adcbf25259e75b47
rachelkathleen <rachel.hawa@gmail.com>
2019-09-30 22:14:51 -0500

    remove margin

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class About extends Component {
     render() {
         return (
-          <div style={{marginTop: '100px'}}>
+          <div>
             <h1>blank space</h1>
             about page
           </div>
