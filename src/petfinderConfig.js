544af7f8b3e7adb20ae4095a71533f02368494f9
rachelkathleen <rachel.hawa@gmail.com>
2019-10-01 01:38:08 -0500

    update client id

--- src/petfinderConfig.js
+++ src/petfinderConfig.js
@@ -1,8 +1,8 @@
 import { Client } from '@petfinder/petfinder-js';
 
 const client = new Client({
-	apiKey: 'hUqDH8B26YwHadBHYbplwsLg9fyNExrYMZ1fYOXRj7oWh5Fmvq',
-	secret: 'lgy0f0Emuw68B5EBYOTlLtITpvyl1eIatXqZqnNR'
+	apiKey: 'Bj8UlWQfyzIv5KBcDzXpnyYfVOqp26AIxWAH17Wla7yYiyfkcq',
+	secret: 'DOSJMI3NuylsXfs3GuGYGcv1AqwCZmmSPN29UnRr'
 });
 
 export default client;
