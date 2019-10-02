ac4533751937e32275214aa270723615c7dd17e2
rachelkathleen <rachel.hawa@gmail.com>
2019-10-02 17:22:38 -0500

    created functional component

---
+++ src/components/AboutText.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function AboutText() {
+    return (
+      <>
+        <p>
+            Pet Roulette! is the final portfolio project by <a href="www.rachel-hawa.com">Rachel Hawa</a>&nbsp;
+            for the <a href="https://flatironschool.com">Flatiron School</a> &nbsp;software engineering immersive.
+        </p>
+        <p>
+            It was made with a <a href="">Ruby on Rails</a> backend, and a&nbsp;  
+            <a href="https://github.com/rachelkathleen/pet-roulette-client">React/Redux</a>
+            &nbsp;front-end.
+        </p>
+        <p>
+            It includes data obtained from the <a href="https://www.petfinder.com/developers/v2/docs/">Petfiner API</a>,
+            is hosted on <a href="heroku.com"></a> and uses <a href="https://guides.rubyonrails.org/action_mailer_basics.html">
+            Rails Action Mailer</a>.
+        </p>
+      </>
+    )
+}
