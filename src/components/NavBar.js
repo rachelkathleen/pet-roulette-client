e91747e79ff5b0b7c3025554ea9be708e7e06180
rachelkathleen <rachel.hawa@gmail.com>
2019-10-02 17:23:29 -0500

    add class to links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export default function NavBar() {
     return (
       <Navbar sticky="top" bg="dark" variant="dark" >
         <Navbar.Brand className="nav"><Link to="/">Pet Roulette!</Link></Navbar.Brand>
-        <Nav className="navlink">
+        <Nav className="navlink justify-content-end">
           <Nav.Link><Link to="/about">ABOUT</Link></Nav.Link>
           <Nav.Link><Link to="/Pets">PETS</Link></Nav.Link>
         </Nav>
