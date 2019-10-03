e7056a70391ec170206191a240a976181b36490a
rachelkathleen <rachel.hawa@gmail.com>
2019-10-02 23:42:12 -0500

    reformat

--- src/components/CardImage.js
+++ src/components/CardImage.js
@@ -1,48 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import Card from 'react-bootstrap/Card'
+import Card from "react-bootstrap/Card";
 import { fetchRandomPet } from "../redux/actions/pets";
-import PetInfo from './PetInfo'
+import PetInfo from "./PetInfo";
 
-class CardImage extends Component {
-
-  render() {
-    const loading = this.props.loading;
-    const pet = this.props.pet;
-    if (loading) {
-        return (
-          <>
-            <Card.Img 
-              className='reframe'  
-              src="https://66.media.tumblr.com/tumblr_lztjlvnBoa1qcay1ao1_500.gif"
-              alt=""
-             />
-          </>
-        );
-    } 
- 
-    return (
+class CardImage extends Component {
+  render() {
+    const loading = this.props.loading;
+    const pet = this.props.pet;
+    if (loading) {
+      return (
         <>
-          <a 
-            href={pet.url}>
-            <Card.Img
-              className='reframe'
-              src={pet.photos[0].medium}
-            />
-          </a>
-          <Card.Body>
-            <PetInfo /> 
-          </Card.Body>
-          </> 
-      )
-    } 
+          <Card.Img
+            className="reframe"
+            src="https://66.media.tumblr.com/tumblr_lztjlvnBoa1qcay1ao1_500.gif"
+            alt=""
+          />
+        </>
+      );
+    }
+    return (
+      <>
+        <a href={pet.url}>
+          <Card.Img className="reframe" src={pet.photos[0].medium} />
+        </a>
+        <Card.Body>
+          <PetInfo />
+        </Card.Body>
+      </>
+    );
+  }
 }
 
-const mapStateToProps = state => { 
-  return { 
+const mapStateToProps = state => {
+  return {
     pet: state.petReducer.pet,
     loading: state.petReducer.loading
-  };
+  };
 };
 
-export default connect(mapStateToProps, { fetchRandomPet })(CardImage);
+export default connect(
+  mapStateToProps,
+  { fetchRandomPet }
+)(CardImage);
--- src/components/PetInfo.js
+++ src/components/PetInfo.js
@@ -1,45 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchRandomPet } from "../redux/actions/pets";
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import EmailForm from './EmailForm'
-
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import EmailForm from "./EmailForm";
 
 class PetInfo extends Component {
-  shufflePet = event => { 
+  shufflePet = event => {
     event.preventDefault();
     this.props.fetchRandomPet();
   };
 
-  render() { 
+  render() {
     return (
-            <>
-              <div style={{backgroundColor: "white"}}>
-                <Row>
-                  <Col>
-                    <button 
-                      className="button-primary" 
-                      onClick={ event => this.shufflePet(event) }>
-                      Shuffle!
-                    </button>
-                  </Col>
-                  <Col>
-                    <EmailForm />
-                  </Col>
-                </Row>
-              </div>
-            </>
-        )
-    }
-
-
+      <>
+        <div style={{ backgroundColor: "white" }}>
+          <Row>
+            <Col>
+              <button
+                className="button-primary"
+                onClick={event => this.shufflePet(event)}
+              >
+                Shuffle!
+              </button>
+            </Col>
+            <Col>
+              <EmailForm shufflePet={this.shufflePet} />
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
+  }
 }
 
-const mapStateToProps = state => { 
-  return { 
-    pet: state.petReducer.pet,
+const mapStateToProps = state => {
+  return {
+    pet: state.petReducer.pet
   };
 };
 
-export default connect(mapStateToProps, { fetchRandomPet })(PetInfo);
+export default connect(
+  mapStateToProps,
+  { fetchRandomPet }
+)(PetInfo);
