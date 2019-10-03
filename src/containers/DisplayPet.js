d3eaf19eb208e90712e267d2c656e57a2e211ce0
rachelkathleen <rachel.hawa@gmail.com>
2019-10-02 23:43:55 -0500

    reformat

--- src/containers/DisplayPet.js
+++ src/containers/DisplayPet.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react'
-import Card from 'react-bootstrap/Card'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import CardImage from '../components/CardImage'
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CardImage from "../components/CardImage";
 
-class DisplayPet extends Component {
+class DisplayPet extends Component {
+  render() {
+    return (
+      <Container style={{ marginTop: "25px" }}>
+        <Row className="justify-content-md-center">
+          <Col md="auto" style={{ width: "30rem" }}>
+            <Card border="dark" style={{ width: "25rem" }} className="reframe">
+              <CardImage />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
-  render() { 
-   return (
-    <Container style={{marginTop: '25px'}}>
-      <Row className="justify-content-md-center">
-        <Col md='auto' style={{ width: '30rem'}}>
-          <Card border="dark" style={{ width: '25rem'}} className='reframe'>
-            <CardImage />
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-      );
-    } 
- }
-
-export default DisplayPet;
+export default DisplayPet;
