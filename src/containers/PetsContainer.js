2021556d394a05934277bea124eaaff2eb0cdc4d
rachelkathleen <rachel.hawa@gmail.com>
2019-10-02 22:19:01 -0500

    create cards to display pet index

--- src/containers/PetsContainer.js
+++ src/containers/PetsContainer.js
@@ -1,20 +1,34 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
 class PetsContainer extends React.Component {
-  render() { debugger
-   const loading = this.props.loading;
+  render() {
+    const loading = this.props.loading;
     if (loading) {
       return <p>Loading...</p>;
     }
+
+    const pet = this.props.pets.reverse().map(p => {
+      return (
+        <div
+          className="card"
+          style={{ width: 15 + "rem", padding: 10, margin: 10 }}
+        >
+          <img
+            className="card-img-top"
+            style={{ maxWidth: 14 + "rem", maxHeight: 14 + "rem" }}
+            src={
+              p.photo
+                ? p.photo
+                : "https://i.ibb.co/YyGLcqn/Screen-Shot-2019-10-02-at-10-09-23-PM.png"
+            }
+          />
+        </div>
+      );
+    });
     return (
-      <div>
-        <h1>Pets List</h1>
-        {this.props.pets.map(pet => (
-          <p key={pet.id}>
-            {pet.name}
-          </p>
-        ))}
+      <div className="row" style={{ margin: 5, alignContent: "center" }}>
+        {pet}
       </div>
     );
   }
