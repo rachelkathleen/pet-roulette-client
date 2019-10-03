c259573861783c7d419e9bcb892e374c3ee03e49
rachelkathleen <rachel.hawa@gmail.com>
2019-10-02 23:42:22 -0500

    reformat

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,50 +1,52 @@
-import React, { Component } from 'react'
-import Modal from 'react-modal';
-import SendPet from './SendPet'
- 
+import React, { Component } from "react";
+import Modal from "react-modal";
+import SendPet from "./SendPet";
+
 const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)"
   }
 };
 
-Modal.setAppElement('#root')
- 
+Modal.setAppElement("#root");
+
 class EmailForm extends Component {
   constructor(props) {
     super(props);
- 
+
     this.state = {
       modalIsOpen: false
     };
- 
+
     this.openModal = this.openModal.bind(this);
     this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
- 
+
   openModal() {
-    this.setState({modalIsOpen: true});
+    this.setState({ modalIsOpen: true });
   }
- 
+
   afterOpenModal() {
     // references are now sync'd and can be accessed.
     // this.subtitle.style.color = 'cyan';
   }
- 
+
   closeModal() {
-    this.setState({modalIsOpen: false});
+    this.setState({ modalIsOpen: false });
   }
- 
+
   render() {
     return (
       <div>
-        <button className="button-primary" onClick={this.openModal}>Share me!</button>
+        <button className="button-primary" onClick={this.openModal}>
+          Share me!
+        </button>
         <Modal
           isOpen={this.state.modalIsOpen}
           onAfterOpen={this.afterOpenModal}
@@ -52,15 +54,18 @@ class EmailForm extends Component {
           style={customStyles}
           contentLabel="Email Form"
         >
-          <button className="button-primary" onClick={this.closeModal} aria-label="Close">
+          <button
+            className="button-primary"
+            onClick={this.closeModal}
+            aria-label="Close"
+          >
             <span aria-hidden="true">&times;</span>
           </button>
-          <SendPet closeModal={this.closeModal}/>
+          <SendPet closeModal={this.closeModal} />
         </Modal>
       </div>
     );
   }
 }
 
-
-export default EmailForm;
+export default EmailForm;
