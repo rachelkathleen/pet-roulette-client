eb549f463a487f60e703f7a5388f9d53ab614e75
rachelkathleen <rachel.hawa@gmail.com>
2019-10-04 00:05:16 -0500

    format changes

--- src/components/DogVsCat.js
+++ src/components/DogVsCat.js
@@ -5,18 +5,17 @@ import moment from "moment";
 class DogVsCat extends Component {
   render() {
     const pets = this.props.pets;
-
     const today = moment().endOf("day");
     const week = today.subtract(1, "w");
     const petsThisWeek = pets.filter(p => week < moment(p.created_at));
     const dogs = petsThisWeek.filter(pet => pet.species === "Dog").length;
     const cats = petsThisWeek.filter(pet => pet.species === "Cat").length;
-    debugger;
 
     return (
       <div>
-        <p>Number of Dogs: {dogs}</p>
-        <p>Number of Cats: {cats}</p>
+        <p>
+          Number of Dogs: {dogs}, Number of Cats: {cats}
+        </p>
       </div>
     );
   }
