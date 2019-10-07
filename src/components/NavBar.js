3ef9b59868fdd81d1e43c4c4f7953f17fc739fff
rachelkathleen <rachel.hawa@gmail.com>
2019-10-06 23:54:15 -0500

    fix layout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavBar = () => (
-  <span className="navbar">
-    <div className="nav">
+  <Navbar className="navbar">
+    <Navbar.Brand className="nav">
       <Link to="/">Pet Roulette!</Link>
-    </div>
+    </Navbar.Brand>
 
-    <div className="ml-auto navlink">
-      <Link to="/pets">GALLERY&emsp;</Link>
-      <Link to="/about">ABOUT&emsp;</Link>
-    </div>
-  </span>
+    <Nav className="ml-auto navlink">
+      <Nav.Link>
+        <Link to="/pets">GALLERY&emsp;</Link>
+      </Nav.Link>
+      <Nav.Link>
+        <Link to="/about">ABOUT&emsp;</Link>
+      </Nav.Link>
+    </Nav>
+  </Navbar>
 );
 
 export default NavBar;
