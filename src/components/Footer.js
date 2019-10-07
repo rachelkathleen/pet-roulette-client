38949bc9be167e59b1ee27847e50b7e92bff6f93
rachelkathleen <rachel.hawa@gmail.com>
2019-10-06 23:05:06 -0500

    refactor code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,21 @@
 import React from "react";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <a href="https://github.com/rachelkathleen" target="blank">
-        {" "}
-        <i className="fab fa-github fa-lg"></i>
-      </a>
+const Footer = () => (
+  <footer className="footer">
+    <a href="https://github.com/rachelkathleen" target="blank">
+      {" "}
+      <i className="fab fa-github fa-lg">&emsp;</i>
+    </a>
 
-      <a href="https://medium.com/@rachel.hawa" target="blank">
-        {" "}
-        <i className="fab fa-medium fa-lg"></i>
-      </a>
+    <a href="https://medium.com/@rachel.hawa" target="blank">
+      {" "}
+      <i className="fab fa-medium fa-lg">&emsp;</i>
+    </a>
 
-      <a href="https://www.linkedin.com/in/rachelhawa/" target="blank">
-        <i className="fab fa-linkedin fa-lg"></i>
-      </a>
-    </footer>
-  );
-}
+    <a href="https://www.linkedin.com/in/rachelhawa/" target="blank">
+      <i className="fab fa-linkedin fa-lg">&emsp;</i>
+    </a>
+  </footer>
+);
+
+export default Footer;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 import { Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function NavBar() {
-  return (
-    <Navbar sticky="top" bg="dark" variant="dark">
-      <Navbar.Brand className="nav">
-        <Link to="/">Pet Roulette!</Link>
-      </Navbar.Brand>
+const NavBar = () => (
+  <Navbar sticky="top" bg="dark" variant="dark">
+    <Navbar.Brand className="nav">
+      <Link to="/">Pet Roulette!</Link>
+    </Navbar.Brand>
 
-      <Nav className="ml-auto navlink">
-        <Nav.Link>
-          <Link to="/pets">GALLERY&emsp;</Link>
-        </Nav.Link>
-        <Nav.Link>
-          <Link to="/about">ABOUT&emsp;</Link>
-        </Nav.Link>
-      </Nav>
-    </Navbar>
-  );
-}
+    <Nav className="ml-auto navlink">
+      <Nav.Link>
+        <Link to="/pets">GALLERY&emsp;</Link>
+      </Nav.Link>
+      <Nav.Link>
+        <Link to="/about">ABOUT&emsp;</Link>
+      </Nav.Link>
+    </Nav>
+  </Navbar>
+);
+
+export default NavBar;
