b4f1d67d0de8ef16f5e88447fcd1e3b44e93cf88
rachelkathleen <rachel.hawa@gmail.com>
2019-10-06 23:45:17 -0500

    make navbar mobile responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavBar = () => (
-  <Navbar sticky="top" bg="dark" variant="dark">
-    <Navbar.Brand className="nav">
+  <div className="navbar">
+    <span className="nav">
       <Link to="/">Pet Roulette!</Link>
-    </Navbar.Brand>
+    </span>
 
-    <Nav className="ml-auto navlink">
-      <Nav.Link>
-        <Link to="/pets">GALLERY&emsp;</Link>
-      </Nav.Link>
-      <Nav.Link>
-        <Link to="/about">ABOUT&emsp;</Link>
-      </Nav.Link>
-    </Nav>
-  </Navbar>
+    <span className="ml-auto navlink">
+      <Link to="/pets">GALLERY&emsp;</Link>
+      <Link to="/about">ABOUT&emsp;</Link>
+    </span>
+  </div>
 );
 
 export default NavBar;
