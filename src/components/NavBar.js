5a4adb759824d3c8b15730390aa64619b096237b
rachelkathleen <rachel.hawa@gmail.com>
2019-10-06 23:51:31 -0500

    update nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavBar = () => (
-  <div className="navbar">
-    <span className="nav">
+  <span className="navbar">
+    <div className="nav">
       <Link to="/">Pet Roulette!</Link>
-    </span>
+    </div>
 
-    <span className="ml-auto navlink">
+    <div className="ml-auto navlink">
       <Link to="/pets">GALLERY&emsp;</Link>
       <Link to="/about">ABOUT&emsp;</Link>
-    </span>
-  </div>
+    </div>
+  </span>
 );
 
 export default NavBar;
