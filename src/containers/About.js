417128110e0db04e0cb947f6e9574e80bdc473b2
rachelkathleen <rachel.hawa@gmail.com>
2019-10-08 16:57:24 -0500

    update styling

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
-import Container from 'react-bootstrap/Container'
-import ContactForm from '../components/ContactForm'
-import AboutText from '../components/AboutText'
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import ContactForm from "../components/ContactForm";
+import AboutText from "../components/AboutText";
 
 export default class About extends Component {
-    render() {
-        return (
-          <Container style={{marginTop: '25px', textAlign:'left'}}>
-            <AboutText />
-            <ContactForm />
-          </Container>
-        );
-      }
+  render() {
+    return (
+      <Container
+        style={{ alignContent: "center", padding: "2em", textAlign: "left" }}
+      >
+        <AboutText />
+        <ContactForm />
+      </Container>
+    );
+  }
 }
-
--- src/containers/PetsContainer.js
+++ src/containers/PetsContainer.js
@@ -1,21 +1,15 @@
 import React from "react";
 import DogVsCat from "../components/DogVsCat";
 import PetGallery from "../components/PetGallery";
+import Container from "react-bootstrap/Container";
 
 class PetsContainer extends React.Component {
   render() {
     return (
-      <>
-        <div
-          style={{
-            margin: "25px",
-            alignContent: "center"
-          }}
-        >
-          <DogVsCat />
-          <PetGallery />
-        </div>
-      </>
+      <Container style={{ alignContent: "center", padding: "2em" }}>
+        <DogVsCat />
+        <PetGallery />
+      </Container>
     );
   }
 }
