66d6dcd7e1b06c3dcd3a0fa93f49085e659a0474
rachelkathleen <rachel.hawa@gmail.com>
2019-10-07 20:11:31 -0500

    remove unused import

--- src/containers/PetsContainer.js
+++ src/containers/PetsContainer.js
@@ -1,13 +1,17 @@
 import React from "react";
 import DogVsCat from "../components/DogVsCat";
 import PetGallery from "../components/PetGallery";
-import Container from "react-bootstrap/Container";
 
 class PetsContainer extends React.Component {
   render() {
     return (
       <>
-        <div style={{ marginTop: "25px", alignContent: "center" }}>
+        <div
+          style={{
+            margin: "25px",
+            alignContent: "center"
+          }}
+        >
           <DogVsCat />
           <PetGallery />
         </div>
