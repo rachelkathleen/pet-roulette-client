191fbe6e19412c6e46cd55552da11f3a969f0eff
rachelkathleen <rachel.hawa@gmail.com>
2019-10-07 21:00:20 -0500

    fix css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Row from "react-bootstrap/Row";
 import "./navandfooter.css";
 
