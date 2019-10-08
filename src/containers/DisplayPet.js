bbeeba5875630f84cdf28517a37c43fe0cf1ae6c
rachelkathleen <rachel.hawa@gmail.com>
2019-10-08 16:35:47 -0500

    minor change

--- src/containers/DisplayPet.js
+++ src/containers/DisplayPet.js
@@ -9,7 +9,7 @@ import { ToastContainer, toast } from "react-toastify";
 class DisplayPet extends Component {
   render() {
     return (
-      <Container style={{ alignContent: "center", padding: "3em" }}>
+      <Container style={{ alignContent: "center", padding: "2.5em" }}>
         <ToastContainer
           enableMultiContainer
           containerId={"pet"}
