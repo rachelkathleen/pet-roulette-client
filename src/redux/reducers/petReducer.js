418ecf35efe9d83c9866fbaae73cceea76c8e488
rachelkathleen <rachel.hawa@gmail.com>
2019-10-16 22:36:14 -0500

    add add_like_success case

--- src/redux/reducers/petReducer.js
+++ src/redux/reducers/petReducer.js
@@ -25,6 +25,17 @@ export default (
       };
     case "FETCH_PETS_SUCCESS":
       return { ...state, all: action.pets };
+    case "ADD_LIKE_SUCCESS":
+      return {
+        ...state,
+        all: state.map(pet => {
+          if (pet.id === action.payload.id) {
+            return action.payload;
+          } else {
+            return pet;
+          }
+        })
+      };
     default:
       return state;
   }
