aedcc75cbf37485eba285029aa9d9b20e331558c
rachelkathleen <rachel.hawa@gmail.com>
2019-10-20 23:20:22 -0500

    update filter buttons to make mobile responsive

--- src/components/PetGalleryFilter.js
+++ src/components/PetGalleryFilter.js
@@ -4,28 +4,18 @@ import Row from "react-bootstrap/Row";
 
 const PetGalleryFilter = props => (
   <div>
-    <Row className="justify-content-md-center">
-      <Col>
-        <span></span>
-      </Col>
-      <Col>
-        <button className="button-primary" onClick={props.dogState}>
-          Dogs: {props.dogs.length}
-        </button>
-      </Col>
-      <Col>
-        <button className="button-primary" onClick={props.allPetsState}>
-          All Pets!
-        </button>
-      </Col>
-      <Col>
-        <button className="button-primary" onClick={props.catState}>
-          Cats: {props.cats.length}
-        </button>
-      </Col>
-      <Col>
-        <span></span>
-      </Col>
+    <Row className="row">
+      <button className="button-primary" onClick={props.dogState}>
+        Dogs: {props.dogs.length}
+      </button>
+
+      <button className="button-primary" onClick={props.allPetsState}>
+        All Pets!
+      </button>
+
+      <button className="button-primary" onClick={props.catState}>
+        Cats: {props.cats.length}
+      </button>
     </Row>
   </div>
 );
