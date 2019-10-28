14ad548d035cd39b7e068b80eb5d003dbc18e36a
rachelkathleen <rachel.hawa@gmail.com>
2019-10-27 19:16:14 -0500

    create component for gallery loading

---
+++ src/components/GalleryLoading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const GalleryLoading = () => (
+  <Container style={{ alignContent: "center", padding: "2em" }}>
+    <img
+      src="http://giphygifs.s3.amazonaws.com/media/Ko55FHr8B2Zzi/giphy.gif"
+      alt="loading puppy"
+    />
+  </Container>
+);
+
+export default GalleryLoading;
