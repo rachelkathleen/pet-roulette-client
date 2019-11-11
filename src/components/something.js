f5d02bbb21cb5d79cd1a4fe120d7aa3175a29cd7
rachelkathleen <rachel.hawa@gmail.com>
2019-11-10 21:19:23 -0600

    update

--- src/components/something.js
+++ src/components/something.js
@@ -5,12 +5,3 @@
             { state = { on: true } 
             changeState = () => { this.setState({on: !this.state.on } ) }; 
             
-            render () { 
-                const text = this.state.on ? this.props.true : this.props.false 
-                const color = this.state.on ? "button on" : "button off" 
-            return (
-              <div>
-                 <div className={color} onClick={this.changeState}>
-                    <div className="text">{text}</div>
-                    <div className="circle"> </div>
-     
