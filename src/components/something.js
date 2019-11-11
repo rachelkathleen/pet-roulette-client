2aa33afa59add1da3df155233398392a23a7b389
rachelkathleen <rachel.hawa@gmail.com>
2019-11-10 19:07:29 -0600

    add file

---
+++ src/components/something.js
@@ -0,0 +1,25 @@
+<body>
+    <div id="root"></div>
+    <script type="text/jsx">
+        class Toggle extends React.Component 
+            { state = { on: true } 
+            changeState = () => { this.setState({on: !this.state.on } ) }; 
+            
+            render () { 
+                const text = this.state.on ? this.props.true : this.props.false 
+                const color = this.state.on ? "button on" : "button off" 
+            return (
+              <div>
+                 <div className={color} onClick={this.changeState}>
+                    <div className="text">{text}</div>
+                    <div className="circle"> </div>
+                </div>
+              </div>
+            ) 
+          } 
+        } 
+    
+    ReactDOM.render(
+        <Toggle true="hi" false="bye" />, document.getElementById('root') );
+    </script>
+</body>
